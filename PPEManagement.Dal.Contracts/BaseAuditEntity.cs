25216cc56a4d1c2c21442500d47a954cd0a2503d
Игорь <igorgrebenyuk2008@gmail.com>
2026-09-25 20:05:39 +0300

    Создал абстрактный класс базовых аудитов для сущностей , чтобы не писать одни и те же поля в сущностях , а так же создал 1 солюшн папку для разработки прослойки между БД и сервисами + создал библиотеку классов для моих сущностей

---
+++ PPEManagement.Dal.Contracts/BaseAuditEntity.cs
@@ -0,0 +1,37 @@
+﻿namespace PPEManagement.Dal.Contracts;
+
+    /// <summary>
+    /// Базовый класс для аудит-сущностей
+    /// </summary>
+    public abstract class BaseAuditEntity
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        /// <summary>
+        /// Когда создан
+        /// </summary>
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+
+        /// <summary>
+        /// Кем создан
+        /// </summary>
+        public string CreatedBy { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Когда изменён
+        /// </summary>
+        public DateTimeOffset UpdatedAt { get; set; } = DateTimeOffset.UtcNow;
+
+        /// <summary>
+        /// Кем изменён
+        /// </summary>
+        public string UpdatedBy { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Дата удаления (для Soft Delete)
+        /// </summary>
+        public DateTimeOffset? DeletedAt { get; set; }
+    }
